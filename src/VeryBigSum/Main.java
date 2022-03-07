00ca48c6a8de7bbf15ebd153e5dda9e9bb583fc0
ivantarpanov <ivantarpanov94@gmail.com>
2022-03-07 21:11:29 +0200

    first commit

---
+++ src/VeryBigSum/Main.java
@@ -0,0 +1,32 @@
+package VeryBigSum;
+
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+//In this challenge, you are required to calculate and print the sum of the elements in an array,
+// keeping in mind that some of those integers may be quite large.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Long> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Long::parseLong)
+                .collect(toList());
+
+        long result = Result.aVeryBigSum(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
