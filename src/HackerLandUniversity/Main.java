24511ca52e00db54795da313cdb420427009b125
ivantarpanov <ivantarpanov94@gmail.com>
2022-03-14 19:19:39 +0200

    first commit

---
+++ src/HackerLandUniversity/Main.java
@@ -0,0 +1,45 @@
+package HackerLandUniversity;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+//HackerLand University has the following grading policy:
+//Every student receives a grade  in the inclusive range from 0 to 100.
+//Any grade less than 40 is a failing grade.
+//Sam is a professor at the university and likes to round each student's grade according to these rules:
+//If the difference between the grade and the next multiple of 5 is less than 3, round grade up to the next multiple of 5.
+//If the value of grade is less than 38, no rounding occurs as the result wil still be a failing grade.
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+                    try {
+                        return bufferedReader.readLine().replaceAll("\\s+$", "");
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
